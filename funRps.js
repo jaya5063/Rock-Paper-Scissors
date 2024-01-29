1fe9609d410712d74d74249a3f6a8dd1d235b701
jaya5063 <jayak5063@gmail.com>
2024-01-29 22:29:52 +0530

    new files

--- funRps.js
+++ funRps.js
@@ -1,8 +1,10 @@
 
 //creation of object
-let score = JSON.parse(localStorage.getItem('score')) ||  {wins:0,
-loses:0,
-ties:0}; // default operator
+let score = JSON.parse(localStorage.getItem('score')) || {
+    wins: 0,
+    loses: 0,
+    ties: 0
+}; // default operator
 
 // if(score===null){
 //     score = {
@@ -18,7 +20,7 @@ function playGame(playerMove) {
     let result = '';
     pickCmove();
     if (playerMove === 'Rock') {
-        
+
         if (computerMove === 'Rock') {
             result = 'it\'s Tie';
         } else if (computerMove === 'Paper') {
@@ -49,23 +51,33 @@ function playGame(playerMove) {
 
     }
 
-    if(result === 'You Win'){
+    if (result === 'You Win') {
         score.wins++;
-    }else if(result === 'You Lose'){
+    } else if (result === 'You Lose') {
         score.loses++;
-    }else if(result ==='it\'s Tie' ){
+    } else if (result === 'it\'s Tie') {
         score.ties++;
     }
-    
-    localStorage.setItem('score',JSON.stringify(score));
 
-    alert(`your choice is ${playerMove} and computer choice is ${computerMove} , ${result}
-    wins:${score.wins},losses:${score.loses},Ties:${score.ties}`);
+    localStorage.setItem('score', JSON.stringify(score));
 
+    document.querySelector('.js-result')
+        .innerHTML = (`${result}`)
     
+    document.querySelector('.js-choices')
+    .innerHTML = (`you:${playerMove}   computer:${computerMove}`)
+
+    document.querySelector('.js-score')
+        .innerHTML = `wins:${score.wins},losses:${score.loses},Ties:${score.ties}`;
+
+
+    // alert(`your choice is ${playerMove} and computer choice is ${computerMove} , ${result}
+    // wins:${score.wins},losses:${score.loses},Ties:${score.ties}`);
+
+
     function pickCmove() {
         const randomNumber = Math.random();
-    
+
         if (randomNumber >= 0 && randomNumber < 1 / 3) {
             computerMove = 'Rock';
         } else if (randomNumber > 1 / 3 && randomNumber < 2 / 3) {
@@ -74,4 +86,11 @@ function playGame(playerMove) {
             computerMove = 'Scissor';
         }
     }
+    // document.querySelector('js-choices')
+    //     .innerHTML = (`you:${playerMove} computer:${computerMove} result:`)
+}
+
+function updateScore() {
+    document.querySelector('.js-score')
+        .innerHTML = `wins:${score.wins},losses:${score.loses},Ties:${score.ties}`;
 }
